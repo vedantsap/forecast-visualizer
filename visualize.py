9df9f7d8eba19251c3ca3b7eebb83ecbe0b43058
Vedant Sapra <vedantsapra@gmail.com>
2023-12-06 16:08:46 -0600

    domain setup

--- visualize.py
+++ visualize.py
@@ -1,12 +1,35 @@
 import streamlit as st
 
-
 st.set_page_config(layout="wide")
 st.title("CS6301 - Visualizer")
-st.subheader("Pranav Jadhav")
-st.subheader("Ravi Kiran Mekala")
-st.subheader("Vedant Sapra")
+
+def main():
+    """
+    Main function to render all elements
+    """
+    tabs = ["Home Page and Readme", "Tab 1", "Tab 2"]
+    tab_home, tab_1, tab_2 = st.tabs(tabs)
+
+    with tab_home:
+        st.header("Home page and about")
+        st.markdown(
+            """
+            Project visualizer for CS6301 - Data Science for Smart Cities, Fall 2023
+            Compelted by:
+                Pranav Jadhav
+                Ravi Kiran Mekala
+                Vedant Sapra
+            """
+        )
+        container_body = st.container()
+        col1, col2 = container_body.columns(2)
+
+        with col1:
+            st.write("50 iterations")
+
+        with col2:
+            st.write("200 iterations")
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
