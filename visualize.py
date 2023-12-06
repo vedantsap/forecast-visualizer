58e47b1e5f29ecceb4b71d59ac12d678a5e0273b
Vedant Sapra <vedantsapra@gmail.com>
2023-12-06 15:42:52 -0600

    domain setup

---
+++ visualize.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+st.title("CS6301 - Visualizer")
+st.subheader("Pranav Jadhav")
+st.subheader("Ravi Kiran Mekala")
+st.subheader("Vedant Sapra")
+
+
+# if __name__ == "__main__":
+#     main()
